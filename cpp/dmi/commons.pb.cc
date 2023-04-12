4ef897bd9729318b5cec9e33c67bda21c7f49e3c
tejaldere <Tejal.Dere@radisys.com>
2023-04-12 10:02:31 +0530

    changes in makefile to support the different version of protoc
    Change-Id: Id6217f775d3ed3fe5517dbf979543850da0dc71e

--- cpp/dmi/commons.pb.cc
+++ cpp/dmi/commons.pb.cc
@@ -5,36 +5,25 @@
 
 #include <algorithm>
 
-#include <google/protobuf/stubs/common.h>
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/extension_set.h>
-#include <google/protobuf/wire_format_lite_inl.h>
+#include <google/protobuf/wire_format_lite.h>
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/generated_message_reflection.h>
 #include <google/protobuf/reflection_ops.h>
 #include <google/protobuf/wire_format.h>
 // @@protoc_insertion_point(includes)
 #include <google/protobuf/port_def.inc>
-
 namespace dmi {
 }  // namespace dmi
-void InitDefaults_dmi_2fcommons_2eproto() {
-}
+static constexpr ::PROTOBUF_NAMESPACE_ID::Metadata* file_level_metadata_dmi_2fcommons_2eproto = nullptr;
+static const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* file_level_enum_descriptors_dmi_2fcommons_2eproto[2];
+static constexpr ::PROTOBUF_NAMESPACE_ID::ServiceDescriptor const** file_level_service_descriptors_dmi_2fcommons_2eproto = nullptr;
+const ::PROTOBUF_NAMESPACE_ID::uint32 TableStruct_dmi_2fcommons_2eproto::offsets[1] = {};
+static constexpr ::PROTOBUF_NAMESPACE_ID::internal::MigrationSchema* schemas = nullptr;
+static constexpr ::PROTOBUF_NAMESPACE_ID::Message* const* file_default_instances = nullptr;
 
-constexpr ::google::protobuf::Metadata* file_level_metadata_dmi_2fcommons_2eproto = nullptr;
-const ::google::protobuf::EnumDescriptor* file_level_enum_descriptors_dmi_2fcommons_2eproto[2];
-constexpr ::google::protobuf::ServiceDescriptor const** file_level_service_descriptors_dmi_2fcommons_2eproto = nullptr;
-const ::google::protobuf::uint32 TableStruct_dmi_2fcommons_2eproto::offsets[1] = {};
-static constexpr ::google::protobuf::internal::MigrationSchema* schemas = nullptr;
-static constexpr ::google::protobuf::Message* const* file_default_instances = nullptr;
-
-::google::protobuf::internal::AssignDescriptorsTable assign_descriptors_table_dmi_2fcommons_2eproto = {
-  {}, AddDescriptors_dmi_2fcommons_2eproto, "dmi/commons.proto", schemas,
-  file_default_instances, TableStruct_dmi_2fcommons_2eproto::offsets,
-  file_level_metadata_dmi_2fcommons_2eproto, 0, file_level_enum_descriptors_dmi_2fcommons_2eproto, file_level_service_descriptors_dmi_2fcommons_2eproto,
-};
-
-const char descriptor_table_protodef_dmi_2fcommons_2eproto[] =
+const char descriptor_table_protodef_dmi_2fcommons_2eproto[] PROTOBUF_SECTION_VARIABLE(protodesc_cold) =
   "\n\021dmi/commons.proto\022\003dmi*\?\n\006Status\022\024\n\020UN"
   "DEFINED_STATUS\020\000\022\r\n\tOK_STATUS\020\001\022\020\n\014ERROR"
   "_STATUS\020\002*\?\n\010LogLevel\022\t\n\005TRACE\020\000\022\t\n\005DEBU"
@@ -42,24 +31,23 @@ const char descriptor_table_protodef_dmi_2fcommons_2eproto[] =
   "thub.com/opencord/device-management-inte"
   "rface/v3/go/dmib\006proto3"
   ;
-::google::protobuf::internal::DescriptorTable descriptor_table_dmi_2fcommons_2eproto = {
-  false, InitDefaults_dmi_2fcommons_2eproto, 
-  descriptor_table_protodef_dmi_2fcommons_2eproto,
-  "dmi/commons.proto", &assign_descriptors_table_dmi_2fcommons_2eproto, 223,
+static const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable*const descriptor_table_dmi_2fcommons_2eproto_deps[1] = {
+};
+static ::PROTOBUF_NAMESPACE_ID::internal::SCCInfoBase*const descriptor_table_dmi_2fcommons_2eproto_sccs[1] = {
+};
+static ::PROTOBUF_NAMESPACE_ID::internal::once_flag descriptor_table_dmi_2fcommons_2eproto_once;
+const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable descriptor_table_dmi_2fcommons_2eproto = {
+  false, false, descriptor_table_protodef_dmi_2fcommons_2eproto, "dmi/commons.proto", 223,
+  &descriptor_table_dmi_2fcommons_2eproto_once, descriptor_table_dmi_2fcommons_2eproto_sccs, descriptor_table_dmi_2fcommons_2eproto_deps, 0, 0,
+  schemas, file_default_instances, TableStruct_dmi_2fcommons_2eproto::offsets,
+  file_level_metadata_dmi_2fcommons_2eproto, 0, file_level_enum_descriptors_dmi_2fcommons_2eproto, file_level_service_descriptors_dmi_2fcommons_2eproto,
 };
 
-void AddDescriptors_dmi_2fcommons_2eproto() {
-  static constexpr ::google::protobuf::internal::InitFunc deps[1] =
-  {
-  };
- ::google::protobuf::internal::AddDescriptors(&descriptor_table_dmi_2fcommons_2eproto, deps, 0);
-}
-
 // Force running AddDescriptors() at dynamic initialization time.
-static bool dynamic_init_dummy_dmi_2fcommons_2eproto = []() { AddDescriptors_dmi_2fcommons_2eproto(); return true; }();
+static bool dynamic_init_dummy_dmi_2fcommons_2eproto = (static_cast<void>(::PROTOBUF_NAMESPACE_ID::internal::AddDescriptors(&descriptor_table_dmi_2fcommons_2eproto)), true);
 namespace dmi {
-const ::google::protobuf::EnumDescriptor* Status_descriptor() {
-  ::google::protobuf::internal::AssignDescriptors(&assign_descriptors_table_dmi_2fcommons_2eproto);
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* Status_descriptor() {
+  ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_dmi_2fcommons_2eproto);
   return file_level_enum_descriptors_dmi_2fcommons_2eproto[0];
 }
 bool Status_IsValid(int value) {
@@ -73,8 +61,8 @@ bool Status_IsValid(int value) {
   }
 }
 
-const ::google::protobuf::EnumDescriptor* LogLevel_descriptor() {
-  ::google::protobuf::internal::AssignDescriptors(&assign_descriptors_table_dmi_2fcommons_2eproto);
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* LogLevel_descriptor() {
+  ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_dmi_2fcommons_2eproto);
   return file_level_enum_descriptors_dmi_2fcommons_2eproto[1];
 }
 bool LogLevel_IsValid(int value) {
@@ -93,10 +81,8 @@ bool LogLevel_IsValid(int value) {
 
 // @@protoc_insertion_point(namespace_scope)
 }  // namespace dmi
-namespace google {
-namespace protobuf {
-}  // namespace protobuf
-}  // namespace google
+PROTOBUF_NAMESPACE_OPEN
+PROTOBUF_NAMESPACE_CLOSE
 
 // @@protoc_insertion_point(global_scope)
 #include <google/protobuf/port_undef.inc>
--- python/dmi/commons_pb2.py
+++ python/dmi/commons_pb2.py
@@ -4,7 +4,6 @@
 """Generated protocol buffer code."""
 from google.protobuf.internal import enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
-from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
@@ -15,11 +14,86 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x11\x64mi/commons.proto\x12\x03\x64mi*?\n\x06Status\x12\x14\n\x10UNDEFINED_STATUS\x10\x00\x12\r\n\tOK_STATUS\x10\x01\x12\x10\n\x0c\x45RROR_STATUS\x10\x02*?\n\x08LogLevel\x12\t\n\x05TRACE\x10\x00\x12\t\n\x05\x44\x45\x42UG\x10\x01\x12\x08\n\x04INFO\x10\x02\x12\x08\n\x04WARN\x10\x03\x12\t\n\x05\x45RROR\x10\x04\x42;Z9github.com/opencord/device-management-interface/v3/go/dmib\x06proto3')
+DESCRIPTOR = _descriptor.FileDescriptor(
+  name='dmi/commons.proto',
+  package='dmi',
+  syntax='proto3',
+  serialized_options=b'Z9github.com/opencord/device-management-interface/v3/go/dmi',
+  create_key=_descriptor._internal_create_key,
+  serialized_pb=b'\n\x11\x64mi/commons.proto\x12\x03\x64mi*?\n\x06Status\x12\x14\n\x10UNDEFINED_STATUS\x10\x00\x12\r\n\tOK_STATUS\x10\x01\x12\x10\n\x0c\x45RROR_STATUS\x10\x02*?\n\x08LogLevel\x12\t\n\x05TRACE\x10\x00\x12\t\n\x05\x44\x45\x42UG\x10\x01\x12\x08\n\x04INFO\x10\x02\x12\x08\n\x04WARN\x10\x03\x12\t\n\x05\x45RROR\x10\x04\x42;Z9github.com/opencord/device-management-interface/v3/go/dmib\x06proto3'
+)
 
-_STATUS = DESCRIPTOR.enum_types_by_name['Status']
+_STATUS = _descriptor.EnumDescriptor(
+  name='Status',
+  full_name='dmi.Status',
+  filename=None,
+  file=DESCRIPTOR,
+  create_key=_descriptor._internal_create_key,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='UNDEFINED_STATUS', index=0, number=0,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='OK_STATUS', index=1, number=1,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='ERROR_STATUS', index=2, number=2,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=26,
+  serialized_end=89,
+)
+_sym_db.RegisterEnumDescriptor(_STATUS)
+
 Status = enum_type_wrapper.EnumTypeWrapper(_STATUS)
-_LOGLEVEL = DESCRIPTOR.enum_types_by_name['LogLevel']
+_LOGLEVEL = _descriptor.EnumDescriptor(
+  name='LogLevel',
+  full_name='dmi.LogLevel',
+  filename=None,
+  file=DESCRIPTOR,
+  create_key=_descriptor._internal_create_key,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='TRACE', index=0, number=0,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='DEBUG', index=1, number=1,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='INFO', index=2, number=2,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='WARN', index=3, number=3,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='ERROR', index=4, number=4,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=91,
+  serialized_end=154,
+)
+_sym_db.RegisterEnumDescriptor(_LOGLEVEL)
+
 LogLevel = enum_type_wrapper.EnumTypeWrapper(_LOGLEVEL)
 UNDEFINED_STATUS = 0
 OK_STATUS = 1
@@ -31,12 +105,10 @@ WARN = 3
 ERROR = 4
 
 
-if _descriptor._USE_C_DESCRIPTORS == False:
+DESCRIPTOR.enum_types_by_name['Status'] = _STATUS
+DESCRIPTOR.enum_types_by_name['LogLevel'] = _LOGLEVEL
+_sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
-  DESCRIPTOR._options = None
-  DESCRIPTOR._serialized_options = b'Z9github.com/opencord/device-management-interface/v3/go/dmi'
-  _STATUS._serialized_start=26
-  _STATUS._serialized_end=89
-  _LOGLEVEL._serialized_start=91
-  _LOGLEVEL._serialized_end=154
+
+DESCRIPTOR._options = None
 # @@protoc_insertion_point(module_scope)
